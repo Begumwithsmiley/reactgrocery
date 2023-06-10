4b9520a4144af0da8e097d8c70c81bac14e2f4c7
Begum SA <begumsener94@gmail.com>
2023-06-10 20:16:54 +0000

    DaisyUI integrated & tested

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
+  <div className="rating gap-1 text-3xl font-bold underline">
+    Hello world!
+    <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+    <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400"  />
+    <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+    <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+    <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
+  </div>
   )
 }
