34ccee1ebf76c078d64bd0c428a9e9bf69befcde
Begum SA <begumsener94@gmail.com>
2023-06-20 09:14:10 +0000

    get chain info

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,7 +2,6 @@
 import Image from 'next/image'
 import { WagmiConfig, createConfig, mainnet } from 'wagmi'
 import { createPublicClient, http } from 'viem'
-
 const config = createConfig({
   autoConnect: true,
   publicClient: createPublicClient({
@@ -15,11 +14,13 @@ export default function App() {
   return (
     <WagmiConfig config={config}>
       <Profile />
+      
     </WagmiConfig>
   )
 }
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useConnect, useDisconnect, useNetwork } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
+import { getChainId } from 'viem/dist/types/actions/public/getChainId';
 
 function Profile() {
   const { address, isConnected } = useAccount()
@@ -27,26 +28,45 @@ function Profile() {
     connector: new InjectedConnector(),
   })
   const { disconnect } = useDisconnect()
+  
+  const { chain, chains } = useNetwork()
+
+
 
   if (isConnected)
     return (
+          
       <div>
-        Connected to {address} <br></br>
+        
         <button className="btn btn-secondary" onClick={() => disconnect()}>Disconnect</button>
+        <div className="hero min-h-screen bg-base-100">
+        <div className="hero-content text-center">
+        <div className="max-w-md">
+          <p className="py-3">Let's join to adventure with us. </p>
+        <div className="rating gap-1 text-2xl font-bold underline">
+      
+        Hello world!
+        <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400"  />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
+        <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
+    
       </div>
+      </div>
+      </div>
+      
+          Connected to {address}  <br></br>
+         
+      </div>
+      {chain && <div>Connected to {chain.name}</div>}
+        {chains && (
+           <div>Available chains: {chains.map((chain) => chain.name)} 
+           </div>
+        )}
+    </div>
+    
     )
-  return <button className="btn btn-secondary" onClick={() => connect()}>Connect Wallet</button>
+  return <button className="btn btn-primary" onClick={() => connect()}>Connect Wallet</button>
 }
 
-function Home() {
-  return (
-  <div className="rating gap-1 text-3xl font-bold underline">
-    Hello world!
-    <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-    <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400"  />
-    <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
-    <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
-    <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" />
-  </div>
-  )
-}
